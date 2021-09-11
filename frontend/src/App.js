1f020abb4411659a287a837fd44d9dbfb7e6c8f9
maxamx <maxamx@gmail.com>
2021-09-11 17:03:11 -0300

    Criação componentes estaticos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import NavBar from './components/navbar/index';
+import DataTable from './components/DataTable';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 function App() {
   return (
     <>
       <NavBar />
-      <div>
+      <div className="container">
         <h1 className="text-primary">Olá Mundo</h1>
+        <DataTable/>
       </div>
+
+      <Footer/>
     </>
   );
 }
---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/maxamx" target="_blank" rel="noreferrer">MaxAmx</a></p>
+                <p className="text-light"><small><strong>Max Vendas React Spring Rest</strong><br />
+                    MaxAmx: <a href="https://instagram.com/maxamx" target="_blank" rel="noreferrer">@maxamx</a></small></p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
