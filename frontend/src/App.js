dc56ad8e671fe220b06d806f8deee5a6ce426f30
maxamx <maxamx@gmail.com>
2021-09-11 17:38:38 -0300

    Implementado Charts estaticos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
+import BarCharts from './components/BarCharts';
 import DataTable from './components/DataTable';
+import DonutCharts from './components/DonutCharts';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 function App() {
@@ -6,11 +8,27 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo</h1>
-        <DataTable/>
-      </div>
+        <h1 className="text-primary py-3">Olá Mundo</h1>
+        <div className="row px-3">
 
-      <Footer/>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de vendas</h5>
+            <BarCharts />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Vendas</h5>
+            <DonutCharts />
+          </div>
+        </div>
+
+        <div className="py-3">
+            <h2 className="text-primary">Lista</h2>
+        </div>
+        
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
